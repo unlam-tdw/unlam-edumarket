f019d12e79e07b53e6465063b770ebf0f576f319
miliroggero <roggeromilagros2005@gmail.com>
2025-11-11 11:55:36 -0300

    Actualización calendario + cursos

--- contacto/contacto.js
+++ contacto/contacto.js
@@ -1,46 +1,31 @@
 export class Contacto {
+  constructor() {}
 
-constructor(){}
+  static check() {
+    const formulario = document.getElementById("form-contacto");
 
+    formulario.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const email = document.getElementById("email").value;
+      const telefono = document.getElementById("telefono").value;
+      const mensaje = document.getElementById("mensaje").value;
 
-static check(){
-    const formulario= document.getElementById("form-contacto");
+      if (mensaje.length >= 1000) {
+        console.log("Superó ", mensaje.length);
+      }
 
+      const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/gm;
+      const regexTelefono = /^\d{4}-\d{4}$/gm;
 
+      if (regexEmail.test(email)) {
+        console.log(regexEmail.test(email));
+      }
 
-    formulario.addEventListener('submit', (event) =>{
-        event.preventDefault()
-        const email= document.getElementById("email").value;
-        const telefono= document.getElementById("telefono").value;
-        const mensaje= document.getElementById("mensaje").value;
-
-        if(mensaje.length >=1000)
-        {
-            console.log("Superó ", mensaje.length);
-        }
-
-        const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/gm;
-        const regexTelefono =/^\d{4}-\d{4}$/gm;
-        
-        if(regexEmail.test(email)){
-            console.log(regexEmail.test(email));
-        }
-
-
-          if(regexTelefono.test(telefono)){
-            console.log(regexTelefono.test(telefono));
-        }
-
+      if (regexTelefono.test(telefono)) {
+        console.log(regexTelefono.test(telefono));
+      }
     });
-
-
-
-
-
-
-}
-
-
+  }
 }
 
 Contacto.check();
